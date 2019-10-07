91fc1dbb47b81e1af9da1f3b78306ac886aff1e8
Sasha Royenko <sasharoyenko@gmail.com>
2019-10-07 21:58:34 +0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -10,7 +10,7 @@ function setup() {
   background(1);
   // Start a socket connection to the server
   // Some day we would run this server somewhere else
-  socket = io.connect('http://localhost:3000');
+  socket = io.connect('https://bort1k.github.io/SocketLab/');
   // We make a named event called 'mouse' and write an
   // anonymous callback function
   socket.on('mouse',
